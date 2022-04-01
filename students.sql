2db5b17ec19565333655d8deacb29cf8cf1025f5
Emmanuel Simasiku <mukumbutasimasiku@gmail.com>
2022-04-01 09:28:47 +0200

    Update students.sql

--- students.sql
+++ students.sql
@@ -1,9 +1,11 @@
 CREATE TABLE STUDENTS(
-   id       INT            NOT NULL,
-   name     CHAR (20)      NOT NULL,
-   age      INT            NOT NULL,
+   id       INT    AUTO INCREMENT    NOT NULL,
+   name     VARCHAR (20)             NOT NULL,
+   age      INT                      NOT NULL,
    address  CHAR (25) ,
-   grade    INT            NOT NULL,  
+   grade    INT                      NOT NULL,  
    class    CHAR(25),     
    primary key (id)
 );
+
+
